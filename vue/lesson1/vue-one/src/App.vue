96695906d55cba0fc4173265a2533021db16de91
jmingzhang <jmingzhang@tencent.com>
2023-06-04 22:49:17 +0800

    vue-one

---
+++ vue/lesson1/vue-one/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import ifshow from './ifshow.vue'
+</script>
+
+<template>
+<p3>模板标签</p3>
+<p>{{ msg }}</p>
+<p>{{ number + 1 }}</p>
+<p>{{ message.split("").reverse().join("") }}</p>
+<p v-html="rawHtml"></p>
+<div v-bind:id="myid" :class="myclass"></div>
+<div v-bind="myobject" ></div>
+
+<header>
+    <div>
+      <ifshow />
+    </div>
+  </header>
+
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+      msg : "This is a msg",
+      number : 19,
+      message: "Hello world",
+      rawHtml: "<a href='https://www.baidu.com'>百度</a>",
+      myid : "myid",
+      myclass : "myclass",
+      myobject : {
+        mytmpid : "myid",
+        mytmpclass : "myclass",
+      },
+    }
+  }
+}
+</script>
---
+++ vue/lesson1/vue-one/src/ifshow.vue
@@ -0,0 +1,37 @@
+<template>
+<p3>条件判断渲染</p3>
+<div v-if="ifflag">if条件渲染你能看见我吗</div>
+<div v-else>ifelse条件渲染你能看见我吗</div>
+<div v-show="showflag">show条件渲染你能看见我吗</div>
+<div v-for="item in names"> {{ item }} </div>
+<div v-for="(value,key) in userInfo"> {{ key }} : {{ value }} </div>
+<button v-on:click="addCount">按钮</button>
+<div>{{ count }}</div>
+</template>
+
+
+
+<script>
+export default {
+    data(){
+        return {
+            showflag : false,
+            ifflag : false,
+            names: ["ABBD","DNFFF","FFWFWF"],
+            userInfo: {
+                name: "jmingzhang",
+                age: "28",
+                sex: "m",
+            },
+            count: 0,
+        }
+    },
+    methods : {
+        addCount(){
+            this.count = this.count + 1;
+        }
+    }
+
+
+}
+</script>
